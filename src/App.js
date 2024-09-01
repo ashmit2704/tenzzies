42e4b0fb2f205c705610b0375720d0c39cf9fa90
Standard Box <ashmit2704@gmail.com>
2024-09-01 20:18:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import './style.css'
+import Die from './Die'
+import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
+
+export default function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)  // check for every element in array and returns true only if everyone is true 
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+        setTenzies(true)
+        // console.log("You won!")
+    }
+  }, [dice])
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie())
+    }
+    return newDice
+  }
+  
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ? {...die, isHeld: !die.isHeld} : die
+    }))
+  }
+
+  const diceElements = dice.map(die => (
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />
+  ))
+
+  function generateNewDie() {
+    return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
+  }
+
+  function rollDice() {
+    if(!tenzies) {
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ? die : generateNewDie()
+      }))
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+  }
+
+  return (
+    <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. 
+        Click each die to freeze it at its current value between rolls.
+      </p>
+      <div className='dice-container'>
+        {diceElements}
+      </div>
+      <button className='dice-btn' onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
+    </main>
+  )
+}
